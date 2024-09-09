1e88463e5517ffb0a545e6c165cd184810e2e076
LuisFelipe333 <luigifelipe333@gmail.com>
2024-09-09 01:18:03 -0600

    Added the sprite change to the object and convert into prefab

--- Assets/Scripts/FallingObject.cs
+++ Assets/Scripts/FallingObject.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class FallingObject : MonoBehaviour
 {
     public float fallSpeed = 2f; // Velocidad de caída
+    public Sprite[] sprites; // Lista de sprites
+    private SpriteRenderer spriteRenderer;
 
     private bool isFalling = false;
     // Start is called before the first frame update
     void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        int objectId = Random.Range(0, sprites.Length);
+        SetSprite(objectId);
+        StartFalling();
+    }
+
+    public void StartFalling()
     {
         isFalling = true;
     }
@@ -19,6 +29,18 @@ public class FallingObject : MonoBehaviour
         if (isFalling)
         {
             transform.position += Vector3.down * fallSpeed * Time.deltaTime; // Movimiento hacia abajo
+        }
+    }
+
+    public void SetSprite(int id)
+    {
+        if (id >= 0 && id < sprites.Length)
+        {
+            spriteRenderer.sprite = sprites[id]; // Asignar el sprite correspondiente
+        }
+        else
+        {
+            Debug.LogWarning("ID fuera de rango, no se asignó ningún sprite.");
         }
     }
 }
